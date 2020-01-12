d3812954d1082f22563dea704a641acbe7e97cba
tomekgora <tomekgora@gmail.com>
2020-01-12 19:08:30 +0100

    changed base url to medical dataset 37852 and typed data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+console.log("START OF INDEX.JS CODE");
 const request = require("superagent");
 const express = require("express");
 const { Router } = require("express");
-const cors 
+const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const router = new Router();
@@ -10,37 +11,40 @@ const port = 4000;
 const app = express();
 
 // Middleware
+corsMiddleware = cors();
 parseMiddleware = bodyParser.json();
+app.use(corsMiddleware);
 app.use(parseMiddleware);
 app.use(router);
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
-
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
 //Forwarding request from App to CSB API and returning result
 
-router
-    .post("POST/data", (req, res) => {
-        console.log("got a request on /data")
-        console.log("this is the request body", req.body)
-        const selection = req.body.selection;
-        const url = 
-            "https://opendata.cbs.nl/ODataApi/odata/70072ned/UntypedDataSet?$filter=((substringof(%27NL%27,RegioS)))&$select=" +
-            selection;
-        console.log("the Url for CBS request", url);
-        request
-            .get(url)
-            .then(cbsApiResponse => {
-                return cbsApiResponse
-            })
-            .catch(err => {
-                console.error(err);
-                res.send({ status: "error in fetch"});
-            })
+router.post("POST/data", (req, res) => {
+  console.log("got a request on /data");
+  console.log("this is the request body", req.body);
+  const selection = req.body.selection;
+  const url =
+    "https://opendata.cbs.nl/ODataApi/odata/37852/TypedDataSet?$select=" +
+    selection;
+  console.log("the Url for CBS request", url);
+  request
+    .get(url)
+    .then(cbsApiResponse => {
+      console.log("This is the CBS response", cbsApiResponse);
+      return cbsApiResponse;
+    })
+    .catch(err => {
+      console.error(err);
+      res.send({ status: "error in fetch" });
+    })
     .res.send(cbsApiResponse)
     .catch(err => {
-        console.error(err)
-        res.send({status: "server error in response"})
-        re
+      console.error(err);
+      res.send({ status: "server error in response" });
+      re;
     });
 });
+
+console.log("END OF INDEX.JS CODE");
