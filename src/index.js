d36b24ed99b16fd431ed9b053a5d76618d4dc2f4
tomekgora <tomekgora@gmail.com>
2020-01-12 21:20:10 +0100

    basic fetch from CBS API via server route working, returned to client

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const request = require("superagent");
 const express = require("express");
 const { Router } = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
+const bodyParser = require("express");
 
 const router = new Router();
 const port = 4000;
@@ -12,38 +12,37 @@ const app = express();
 
 // Middleware
 corsMiddleware = cors();
-parseMiddleware = bodyParser.json();
 app.use(corsMiddleware);
+
+parseMiddleware = bodyParser.json();
 app.use(parseMiddleware);
+
 app.use(router);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 //Forwarding request from App to CSB API and returning result
 
-router.post("POST/data", (req, res) => {
+router.post("/data", (req, res) => {
   console.log("got a request on /data");
   console.log("this is the request body", req.body);
   const selection = req.body.selection;
   const url =
-    "https://opendata.cbs.nl/ODataApi/odata/37852/TypedDataSet?$select=" +
+    "https://opendata.cbs.nl/ODataApi/odata/37852/UntypedDataSet?$select=" +
     selection;
   console.log("the Url for CBS request", url);
   request
     .get(url)
-    .then(cbsApiResponse => {
-      console.log("This is the CBS response", cbsApiResponse);
-      return cbsApiResponse;
+    .then(cbsResponse => {
+      console.log("CBS response received");
+      return cbsResponse;
+    })
+    .then(cbsResponse => {
+      res.send(cbsResponse.body);
     })
     .catch(err => {
       console.error(err);
       res.send({ status: "error in fetch" });
-    })
-    .res.send(cbsApiResponse)
-    .catch(err => {
-      console.error(err);
-      res.send({ status: "server error in response" });
-      re;
     });
 });
 
