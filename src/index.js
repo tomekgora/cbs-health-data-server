ff3db575ec2919cbe8b34d12802f0ca980395bfd
tomekgora <tomekgora@gmail.com>
2020-01-16 16:28:23 +0100

    final push

--- src/index.js
+++ src/index.js
@@ -25,13 +25,13 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 
 //FILTERING: filter=((substringof(%27NL%27,RegioS)))
 
+// $filter=Periods(substring(Key,0,4)ge'1950')
+
 router.post("/data", (req, res) => {
   console.log("got a request on /data");
   console.log("this is the request body", req.body);
   const selection = req.body.selection;
-  const url =
-    "https://opendata.cbs.nl/ODataApi/odata/37852eng/UntypedDataSet?$select=" +
-    selection;
+  const url = `https://opendata.cbs.nl/ODataApi/odata/37852eng/TypedDataSet?$select=${selection}`;
   console.log("the Url for CBS request", url);
   request
     .get(url)
@@ -40,7 +40,7 @@ router.post("/data", (req, res) => {
       return cbsResponse;
     })
     .then(cbsResponse => {
-      res.send(cbsResponse.body);
+      res.send(cbsResponse.body.value.slice(75));
     })
     .catch(err => {
       console.error(err);
